e4a77c4be825295668d93ff53e89078d53435335
Ali Ismail <aliismaildeveloper@gmail.com>
2025-10-08 15:34:47 -0700

    refactor(dependencies): add ragservice

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,4 +1,9 @@
-from app.core.config import Settings
+from fastapi import Depends
+from app.core.config import settings
+from app.services.rag_service import RAGService, get_rag_service
 
 def get_settings():
-    return Settings
+    return settings
+
+def get_rag(rag: RAGService = Depends(get_rag_service)):
+    return rag
